fa53213fea4278e57d74549c5c9d4949ee41cd32
helkv <helk@mpdl.mpg.de>
2019-12-17 10:46:23 +0100

    #3 Add WebDriverWait to the BasePage
    - Add the WebDriverWait to the BasePage, so that Seleniums explicit
    waiting mechanism can be used in the Page Objects
    - Set the maximum waiting time to 10 seconds

--- src/main/java/ui/pages/BasePage.java
+++ src/main/java/ui/pages/BasePage.java
@@ -2,6 +2,7 @@ package ui.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Abstract Page Object encapsulates functionalities all pages share.
@@ -16,8 +17,13 @@ public abstract class BasePage {
 
 	protected WebDriver driver;
 
+	// TODO: Relocate the wait & Rework Waiting-Mechanism for the page loadings
+	protected WebDriverWait wait;
+
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
+
+		wait = new WebDriverWait(driver, 10);
 
 		PageFactory.initElements(this.driver, this);
 	}
